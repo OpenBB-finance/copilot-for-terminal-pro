f85f7e897d053691ccb8fe75f628ee8f7dc88178
Michael Struwig <mnicstruwig@gmail.com>
2024-02-07 17:13:29 +0200

    Format code.

--- example-copilot/test.py
+++ example-copilot/test.py
@@ -6,11 +6,7 @@ BASE_URL = "http://localhost:7777"
 print("\n\nTesting query:")
 print("==============")
 with httpx.stream(
-        "POST",
-        f"{BASE_URL}/query",
-        json={
-            "query": "What is your name?"
-        }
+    "POST", f"{BASE_URL}/query", json={"query": "What is your name?"}
 ) as response:
     for chunk in response.iter_text():
         print(chunk, end="", flush=True)
@@ -20,37 +16,30 @@ with httpx.stream(
 print("\n\nTesting query with context:")
 print("==============")
 with httpx.stream(
-        "POST",
-        f"{BASE_URL}/query",
-        json={
-            "query": "What is the current stock price of TSLA?",
-            "context": "The current price of TSLA is $99.95"
-        }
+    "POST",
+    f"{BASE_URL}/query",
+    json={
+        "query": "What is the current stock price of TSLA?",
+        "context": "The current price of TSLA is $99.95",
+    },
 ) as response:
     for chunk in response.iter_text():
         print(chunk, end="", flush=True)
-
 
 
 # Query, with history.
 print("\n\nTesting query with chat history:")
 print("==============")
 with httpx.stream(
-        "POST",
-        f"{BASE_URL}/query",
-        json={
-            "query": "Ach.",
-            "messages": [
-                {
-                    "role": "human",
-                    "content": "Knock knock..."
-                },
-                {
-                    "role": "ai",
-                    "content": "Who's there?"
-                },
-            ]
-        }
+    "POST",
+    f"{BASE_URL}/query",
+    json={
+        "query": "Ach.",
+        "messages": [
+            {"role": "human", "content": "Knock knock..."},
+            {"role": "ai", "content": "Who's there?"},
+        ],
+    },
 ) as response:
     for chunk in response.iter_text():
         print(chunk, end="", flush=True)
